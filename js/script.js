3e73aa409848158337376df8067765de77c3cf18
Alyssa Karakula <alyssakarakula@Alyssas-MacBook-Air.local>
2020-10-17 16:46:08 -0400

    adds index.html css/ and js/ to weather app

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// constants and variables 
+const{openWeatherAPIKey} = CONFIG;
+
+const API_KEY = CONFIG.openWeatherAPIKey;
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+
+let weatherData;
+// cached elements references
+const $title = $('#title');
+const $temp = $('#temp');
+const $index = $('#index');
+const $desc= $('#desc');
+const $form = $('form');
+const $input = $('input[type="text"]');
+
+
+// Event listeners
+
+$form.on('submit', handleGetData);
+//functions
+
+//q={city name}&appid={API key}
+
+function handleGetData(event) {
+    event.preventDefault();
+
+    const input = $input.val();
+
+    if(!input) return;
+    //'appid=' + API_KEY + '&q=' + input +'&units=imperial'
+    $.ajax(BASE_URL + 'appid=' + API_KEY + '&q=' + input +'&units=imperial') 
+    .then(function(data){
+        //console.log('Data:', data);
+
+        weatherData = data;
+        render(); 
+        
+        
+    }, function(error) {
+        console.log('Error:', error);
+    });
+}
+
+
+function render(){
+    $title.text(`Weather For: ${weatherData.name}`);
+    $temp.html(`Temperature: ${Math.floor(weatherData.main.temp)} &deg;`);
+    $index.html(`Feels Like: ${Math.floor(weatherData.main.feels_like)} &deg;`);
+    $desc.text(`Weather: ${weatherData.weather[0]["description"]}`);
+}
+
+//+ 'q=Granby&appid=' + API_KEY
